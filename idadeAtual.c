69423df67ffd6155b4caf96f6dda0d49d850861d
Felipe Othon Santiago <felipeosantiago@gmail.com>
2023-09-08 16:40:50 -0300

    enviar exercicio 2

---
+++ idadeAtual.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int anoNascimento, anoAtual;
+
+    printf ("Informe seu ano de Nascimento ");
+    scanf("%d", &anoNascimento);
+    printf ("Informe o ano atual: ");
+    scanf("%d", &anoAtual);
+    int idade = anoAtual - anoNascimento;
+    int idade2050 = 2050 - anoNascimento;
+
+    printf("A Sua idade atual e: %d anos, e em 2050 voce tera: %d anos ", idade, idade2050);
+
+    return 0;
+
+
+}
+// Created by felip on 08/09/2023.
+//
